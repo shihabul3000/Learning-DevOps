ca95cbc552f6e205ef18a6cb16027cea46b2efaf
Alvi <shihabul72@gmail.com>
2025-12-28 09:18:59 +0600

    'updated'

---
+++ Milestone-5/Prisma-Blog-App/src/modules/post/post.controller.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { postService } from "./post.service";
+
+const createPost = async (req: Request, res: Response) => {
+    try {
+        const result = await postService.createPost(req.body)
+        res.status(201).json(result)
+    } catch (e) {
+        res.status(400).json({
+            error: "Post creation failed",
+            details: e
+        })
+    }
+}
+
+export const PostController = {
+    createPost
+}
