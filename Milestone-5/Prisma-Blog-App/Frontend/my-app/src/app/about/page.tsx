0f5c4019f1806a700b25f75a33b5d536936eec1f
Alvi <shihabul72@gmail.com>
2026-01-12 13:47:32 +0600

    'updated'

---
+++ Milestone-5/Prisma-Blog-App/Frontend/my-app/src/app/about/page.tsx
@@ -0,0 +1,7 @@
+export default function AboutPage(){
+    return(
+        <div>
+            <h1> This is About Page Component</h1>
+        </div>
+    )
+}
---
+++ Milestone-5/Prisma-Blog-App/Frontend/my-app/src/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BrachPage(){
+    return(
+        <div>
+            <h1> This is Brach Page Component</h1>
+        </div>
+    )
+}
